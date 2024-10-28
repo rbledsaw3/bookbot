8c97b9fadca466431a88fbd979304b072b507f4b
Robert Bledsaw III <rbledsaw3@outlook.com>
2024-10-27 20:30:33 -0400

    Added reporting for a summary of word count and character counts.

--- main.py
+++ main.py
@@ -6,20 +6,43 @@ def read_file(file_path):
 def count_words(file_path):
     with open(file_path, 'r') as file:
         words = file.read().split()
-        print(f"Total words: {len(words)}")
+        return len(words)
 
 def count_characters(file_path):
     with open(file_path, 'r') as file:
         text = file.read().lower()
         char_set = set(text)
         char_counts = {char: text.count(char) for char in char_set}
-    return char_counts
+        sorted_char_counts = dict(sorted(char_counts.items()))
+    return sorted_char_counts
+
+def report(file_path):
+    word_count = count_words(file_path)
+    char_counts = count_characters(file_path)
+    print(f"---Begin report of {file_path} ---")
+    print(f"{word_count} words found in the document")
+    print("\n")
+    for char, count in char_counts.items():
+        if char == '\n':
+            char = 'newline'
+        elif char == ' ':
+            char = 'space'
+        elif char == '\t':
+            char = 'tab'
+        elif char == '\r':
+            char = 'carriage return'
+        elif char == '\v':
+            char = 'vertical tab'
+        elif char == '\f':
+            char = 'form feed'
+        print(f"The \'{char}\' character was found {count} times")
+    print("--- End report ---")
+
 
 def main():
     file_path = "books/frankenstein.txt"
-    read_file(file_path)
-    count_words(file_path)
-    print(count_characters(file_path))
+    report(file_path)
+    
 
 
 if __name__ == "__main__":
