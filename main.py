1ff3c88aa8a43c2fbacaecb1121ff459e6bca3a2
Robert Bledsaw III <rbledsaw3@outlook.com>
2024-10-27 20:03:40 -0400

    added function for counting words within txt file

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-with open('books/frankenstein.txt', 'r') as file:
-    for line in file:
-        print(line, end='')
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            print(line, end='')
+
+def count_words(file_path):
+    with open(file_path, 'r') as file:
+        words = file.read().split()
+        print(f"Total words: {len(words)}")
+
+def main():
+    file_path = "books/frankenstein.txt"
+    read_file(file_path)
+    count_words(file_path)
+
+
+if __name__ == "__main__":
+    main()
+
